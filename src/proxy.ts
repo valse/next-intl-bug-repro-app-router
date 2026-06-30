f6e5d2e09c3fe3d9b8730ee67d51ebe6b6fb3dcb
Marco Valsecchi <marco.valsecchi@gmail.com>
2026-06-30 22:50:40 +0200

    Update proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,11 +1,16 @@
 import createMiddleware from "next-intl/middleware";
+import { NextRequest } from "next/server";
 import { routing } from "./i18n/routing";
 
-export default createMiddleware(routing);
+const intlMiddleware = createMiddleware(routing);
+
+export default function middleware(req: NextRequest) {
+  return intlMiddleware(req);
+}
 
 export const config = {
   // Match all pathnames except for
   // - … if they start with `/api`, `/trpc`, `/_next` or `/_vercel`
   // - … the ones containing a dot (e.g. `favicon.ico`)
-  matcher: "/((?!api|trpc|_next|_vercel|.*\\..*).*)",
+  matcher: "/((?!api|_next|.*\\..*).*)",
 };
