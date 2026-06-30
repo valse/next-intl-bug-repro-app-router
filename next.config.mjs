c6aab1e27046d39244883fe611db0aff913f5f3f
Marco Valsecchi <marco.valsecchi@gmail.com>
2026-06-30 22:46:30 +0200

    Add react compiler

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,19 @@
 // @ts-check
 
-import createNextIntlPlugin from 'next-intl/plugin';
- 
+import createNextIntlPlugin from "next-intl/plugin";
+
 const withNextIntl = createNextIntlPlugin();
- 
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
- 
+const nextConfig = {
+  trailingSlash: false,
+  reactStrictMode: false,
+  reactCompiler: true,
+  experimental: {
+    prefetchInlining: true,
+    taint: true,
+    turbopackFileSystemCacheForDev: true,
+  },
+};
+
 export default withNextIntl(nextConfig);
